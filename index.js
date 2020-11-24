847314b5b1aa838eece0b2459f5e4bee62000ad6
danlagos <lagos.d@gmail.com>
2020-11-23 16:09:49 -0800

    identified method in HTTP req.

--- index.js
+++ index.js
@@ -17,11 +17,15 @@ const server = http.createServer(function (req, res) {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  // Get the HTTP method.  making everything lowercase for now
+  const method = req.method.toLowerCase();
+
   // Send the response
   res.end(`Hello World\n`);
 
   // Log the request path
-  console.log(`Request is received on path: ${trimmedPath}`)
+  console.log(`Request is received on path: ${trimmedPath}\n
+  With method: ${method} `)
 });
 
 server.listen(3000, function () {
