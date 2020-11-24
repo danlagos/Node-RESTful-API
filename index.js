5cf8b799631aa11010e720ce5c8c93e1ab37e523
danlagos <lagos.d@gmail.com>
2020-11-23 23:02:18 -0800

    last comit of the day.

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 
 const http = require(`http`);
 const url = require(`url`)
+const StringDecoder = require(`string_decoder`).StringDecoder;
 
 // The server should respond to request with a string
 
-// Start the server, and have it listen to port 3000
-
 const server = http.createServer(function (req, res) {
+
   // Get url and parse it
   const parsedUrl = url.parse(req.url, true);
 
@@ -17,18 +17,57 @@ const server = http.createServer(function (req, res) {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  //  Get the query string as an object - BROKEN
+  const queryStringObject = parsedUrl.query;
+
   // Get the HTTP method.  making everything lowercase for now
   const method = req.method.toLowerCase();
 
-  // Send the response
-  res.end(`Hello World\n`);
+  //Get the header as an object
+  const headers = req.headers;
 
-  // Log the request path
-  console.log(`Request is received on path: ${trimmedPath}\n
-  With method: ${method} `)
+  // get payload if there is any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', function (data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end', function () {
+    buffer += decoder.end();
+
+    // choose the handler this req should go to.  if one is not found use the no found handler
+
+    //  stopped here
+
+    // Send the response
+    res.end(`Hello World\n`);
+
+    // Log the request path
+    console.log(`Request received with payload: ${buffer}`)
+  });
+
 });
 
+// Start the server, and have it listen to port 3000
 server.listen(3000, function () {
   console.log(`The server is lisetining on port 3000 now`)
 });
 
+// Define the handlers
+let handlers = {};
+
+// sample handler
+handlers.sample = function (data, callback) {
+  // callback a http status code, and a payload object
+  callback(406, { 'name ': 'sample handler' });
+};
+
+//  Not found handler
+handlers.notFound = function (data, callback) {
+  callback(404);
+};
+
+// Define a request router
+let router = {
+  'sample': handlers.sample
+}
